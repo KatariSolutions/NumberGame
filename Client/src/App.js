99f39aa41b43509bd7c2f942108b225f35c5412e
KatariSolutions <katarisolutions20@gmail.com>
2026-01-25 16:15:48 +0530

    UI changes 25-01-2026

--- Client/src/App.js
+++ Client/src/App.js
@@ -19,7 +19,7 @@ function App() {
     <>
       <ToastContainer
         position="top-right"
-        autoClose={3000}
+        autoClose={2000}
         hideProgressBar={false}
         newestOnTop={true}
         closeOnClick
